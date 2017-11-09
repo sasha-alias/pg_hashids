a59dbbf128125e71f0b0617c06fd1fcab17e6e65
William Ivanski <william.ivanski@gmail.com>
2017-11-09 12:24:31 -0200

    Reorganizing for a single vagrant machine

--- debian/bootstrap.sh
+++ debian/bootstrap.sh
@@ -22,17 +22,19 @@ fi
 apt-get update
 apt-get -y upgrade
 
-apt-get -y install build-essential git postgresql-server-dev-9.3 libpq-dev
+apt-get -y install build-essential git postgresql-server-dev-9.3 postgresql-server-dev-9.4 postgresql-server-dev-9.5 postgresql-server-dev-9.6 postgresql-server-dev-10 libpq-dev
 
 echo "Cloning pg_hashids repo..."
 rm -rf ~/pg_hashids
 git clone https://github.com/wind39/pg_hashids ~/pg_hashids
 echo "Done"
 
-echo "Building..."
-cd ~/pg_hashids
-make
-make install
-make deb
-mv *.deb debian/
-echo "Done"
+for pgv in 10 9.6 9.5 9.4 9.3
+do
+    echo "Building for PostgreSQL $pgv"
+    cd ~/pg_hashids/debian/postgresql-$pgv
+    make
+    make install
+    make deb
+    echo "Done"
+done
